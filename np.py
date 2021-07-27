e408e140bebbb331751f3ff16667f46072e6b362
Daniel Martin <dpmartin@valcom.com>
2021-07-27 06:08:06 -0400

    implement backprops

--- np.py
+++ np.py
@@ -9,6 +9,9 @@ def create_weights_matrix(numRows, numCols):
         size=(numRows, numCols)
     )
 
+def create_bias_vector(length):
+    return create_weights_matrix(length, 1)
+
 class ActivationFunction:
     @abstractmethod
     def f(x):
@@ -43,10 +46,6 @@ class MSE(LossFunction):
     def dloss(self, outputs, targets):
         return 2 * (outputs - targets) / outputs.size
 
-
-def create_bias_vector(length):
-    return create_weights_matrix(length, 1)
-
 class Layer:
     """
         This class is representing the connections between 2 layers of neurons
@@ -62,14 +61,33 @@ class Layer:
 class Network:
     """Class representing a sequence of compatible layers"""
 
-    def __init__(self, layers):
+    def __init__(self, layers, loss_func):
         self.layers = layers
+        self.loss_func = loss_func
 
     def forward_pass(self, x):
         out = x
         for layer in self.layers:
             out = layer.forward_pass(out)
         return out
+    
+    def train(self, x, target):
+        """Train the network on the input x and the target value"""
+        # accumulate all intermediate outputs
+        xs = [x]
+
+        for layer in self.layers:
+            xs.append(layer.forward_pass(xs[-1]))
+
+        dx = self.loss_func.dloss(xs.pop(), target)
+
+        # zip allows to loop 2 iterables at once
+        for layer, x in zip(self.layers[::-1], xs[::-1]):
+            db = dx * layer.act_func.df(np.dot(layer.w, x) + layer.b)
+            dx = np.dot(layer.w.T, db)
+            dw = np.dot(db, x.T)
+            layer.w -= 0.001 * dw
+            layer.b -= 0.001 * db
         
 
 if __name__ == '__main__':
