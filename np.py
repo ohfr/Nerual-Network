ccb945ad73460a922340ef4c9cfba915c69a3843
Daniel Martin <dpmartin@valcom.com>
2021-07-27 04:58:20 -0400

    added activation function and cost function

--- np.py
+++ np.py
@@ -14,16 +14,20 @@ def create_bias_vector(length):
 def leaky_relu(x, alpha=0.1):
     return np.maximum(alpha*x, x)
 
+def mean_squared_erorr(outputs, targets):
+    np.mean(np.power(outputs - targets, 2))
+
 class Layer:
     """
         This class is representing the connections between 2 layers of neurons
     """
-    def __init__(self, inputs, outputs):
+    def __init__(self, inputs, outputs, act_func):
         self.W = create_weights_matrix(outputs, inputs)
         self.b = create_bias_vector(outputs)
+        self.f = act_func
 
     def forward_pass(self, x):
-        return np.dot(self.W, x) + self.b
+        return self.f(np.dot(self.W, x) + self.b)
 
 class Network:
     """Class representing a sequence of compatible layers"""
@@ -40,9 +44,9 @@ class Network:
 
 if __name__ == '__main__':
     layers = [
-        Layer(3, 7),
-        Layer(7, 6),
-        Layer(6, 2)
+        Layer(3, 7, leaky_relu),
+        Layer(7, 6, leaky_relu),
+        Layer(6, 2, leaky_relu)
     ]
 
     net = Network(layers)
